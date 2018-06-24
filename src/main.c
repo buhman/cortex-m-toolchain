432aeba134c9c8925f312bb2ff41805e47a351ea
Zack Buhman <zack@buhman.org>
2018-06-24 14:07:08 -0700

    external: add cmsis intrinsic functions
    This also experiments with the systick timer hardware.

--- src/main.c
+++ src/main.c
@@ -1,10 +1,24 @@
+#include <stdint.h>
+#include <cmsis_gcc.h>
+
 /* https://github.com/qemu/qemu/blob/master/hw/arm/stellaris.c#L1251 */
-volatile unsigned int * const UART0 = (unsigned int *)0x4000C000;
+volatile uint32_t * const UART0 = (uint32_t *)0x4000C000;
+
+/* http://infocenter.arm.com/help/topic/com.arm.doc.dui0552a/DUI0552A_cortex_m3_dgug.pdf
+   page 159 4-33 */
+volatile uint32_t * const SYST_CSR = (uint32_t *)0xE000E010;
+volatile uint32_t * const SYST_RVR = (uint32_t *)0xE000E014;
+volatile uint32_t * const SYST_VCR = (uint32_t *)0xE000E018;
+volatile uint32_t * const SYST_CALIB = (uint32_t *)0xE000E01C;
+
+#define CLKSOURCE (1 << 2)
+#define TICKINT (1 << 1)
+#define ENABLE (1 << 0)
 
 void write_uart0(const char *str)
 {
   while (*str != '\0') {
-    *UART0 = (unsigned int)(*str);
+    *UART0 = (uint32_t)(*str);
     str++;
   }
 }
@@ -14,4 +28,20 @@ void main(void)
   const char *works = "asdf foo!\n";
 
   write_uart0(works);
+
+  /* use core clock | generate systick ISR | enable counter */
+  uint32_t tick_csr = CLKSOURCE | TICKINT | ENABLE;
+  /* trigger ISR once every tick_rvr cycles */
+  uint32_t tick_rvr = 0x00FFFFFF;
+
+  *SYST_CSR = tick_csr;
+  *SYST_RVR = tick_rvr;
+
+  __enable_irq();
+
+  while (1) {
+    __WFI();
+
+    write_uart0("tick");
+  }
 }
--- src/vector_table.c
+++ src/vector_table.c
@@ -5,19 +5,53 @@ typedef void (* vec_t)(void);
 /* linker symbols */
 extern uint32_t __stack_top;
 
-/* core interrupt handlers */
+/* forward declarations */
 extern void _start(void);
+void default_handler(void);
+void systick_handler(void);
 
-/* lpc interrupt handlers */
-
+/* cortex-m/lpc interrupt handlers */
 const vec_t __vectors[] __attribute__ ((section(".vectors"))) = {
   /* stack pointer */
   (vec_t)(&__stack_top),
   /* reset vector */
   _start,
-  /* more vectors */
+  /* NMI */
+  0,
+  /* hard fault */
+  0,
+  /* MPU fault */
+  0,
+  /* bus fault */
+  0,
+  /* usage fault */
+  0,
+  /* reserved */
+  0,
+  /* reserved */
+  0,
+  /* reserved */
+  0,
+  /* reserved */
+  0,
+  /* svcall */
+  0,
+  /* debug monitor */
+  0,
+  /* reserved */
+  0,
+  /* pendsv */
+  0,
+  /* systick */
+  systick_handler
 };
 
-void default_handler(void) {
+void default_handler(void)
+{
   while (1);
+}
+
+void systick_handler(void)
+{
+  return;
 }
