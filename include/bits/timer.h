726a101618041fbb827ffae716e3adda16763d8b
Zack Buhman <zack@buhman.org>
2018-07-07 22:19:17 -0700

    wip

---
+++ include/bits/timer.h
@@ -0,0 +1,17 @@
+/* TCR */
+#define CEN (1 << 0)
+#define CRST (1 << 1)
+
+/* MCR */
+#define MR0I (1 << 0)
+#define MR0R (1 << 1)
+#define MR0S (1 << 2)
+#define MR1I (1 << 3)
+#define MR1R (1 << 4)
+#define MR1S (1 << 5)
+#define MR2I (1 << 6)
+#define MR2R (1 << 7)
+#define MR2S (1 << 8)
+#define MR3I (1 << 9)
+#define MR3R (1 << 10)
+#define MR3S (1 << 11)
